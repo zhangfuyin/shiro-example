28657b70929aa98da7e3d42186bbbcf6fec1d07b
Fuyin.Zhang <Fuyin.Zhang@movit-tech.com>
2019-08-26 16:16:31 +0800

    ItemCustom修改注释--master

--- src/main/java/com/zhangfy/po/ItemsCustom.java
+++ src/main/java/com/zhangfy/po/ItemsCustom.java
@@ -1,13 +1,7 @@
 package com.zhangfy.po;
 
 /**
- * 
- * <p>Title: ItemsCustom</p>
- * <p>Description:商品信息的扩展类 </p>
- * <p>Company: www.itcast.com</p> 
- * @author	传智.燕青
- * @date	2015-3-20下午2:54:23
- * @version 1.0
+ * 商品信息扩展类
  */
 public class ItemsCustom extends Items{
 
