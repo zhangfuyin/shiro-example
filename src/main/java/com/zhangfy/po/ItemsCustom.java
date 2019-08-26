2cb19c3bac1e67f8a43c2ba151438ec19279a862
Fuyin.Zhang <Fuyin.Zhang@movit-tech.com>
2019-08-26 16:46:47 +0800

    master 修改 -dev

--- src/main/java/com/zhangfy/po/ItemsCustom.java
+++ src/main/java/com/zhangfy/po/ItemsCustom.java
@@ -2,9 +2,12 @@ package com.zhangfy.po;
 
 /**
  * 商品信息扩展类
+ * 这来我增加一行dev
  */
 public class ItemsCustom extends Items{
 
-    // private String ss  // 测试 branch merge
+    // private String ss  // 测试 branch merge dev
+
+    // private String dd  // 测试 branch merge2
 
 }
